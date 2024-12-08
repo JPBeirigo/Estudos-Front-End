ca3769c0520366ad9de8d7fb1e3902bce5377b3f
Joao P F Beirigo <joao.f.beirigo@gmail.com>
2024-12-08 20:44:56 -0300

    add pokeAPI.js, update index.html and main.js

--- aulas_Javascript/projeto-pokedex/assets/js/main.js
+++ aulas_Javascript/projeto-pokedex/assets/js/main.js
@@ -1,20 +1,19 @@
-const offset = 0
-const limit = 10
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokemonTypesToLI(PokemonTypes){
+    return PokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLI (pokemon){
     return `
         <li class="pokemon">
-                <span class="number">#001</span>
+                <span class="number">#${pokemon.order}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">     
                     <ol class="types">
-                        <li class="type">Grass</li>
-                        <li class="type">Poison</li>
+                        ${convertPokemonTypesToLI(pokemon.types).join('')}
                     </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}">
+                    <img src="${pokemon.sprites.other.showdown.front_default}" alt="${pokemon.name}">
                 </div>
             </li>
         `
@@ -22,27 +21,7 @@ function convertPokemonToLI (pokemon){
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLI(pokemon)
-        }
-    }
-    )
-    .catch ((error) => console.error(error))
-
-    /* Outra forma de declarar estas funções
-    .then((response) => {
-        return response.json()
-    })
-    .then(function(jsonBody) {
-        console.log(jsonBody)
-    })
-    .catch (function(error){
-        console.error(error)
-    })
-    */
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHTML = pokemons.map(convertPokemonToLI).join('')
+    pokemonList.innerHTML = newHTML
+})
